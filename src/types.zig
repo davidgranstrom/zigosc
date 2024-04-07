1e155325cf6ef65367838a7c546678905e428e59
David Granström <david@davidgranstrom.com>
2024-04-07 11:36:50 +0200

    Add Message

--- src/types.zig
+++ src/types.zig
@@ -1,6 +1,15 @@
 const std = @import("std");
 const testing = std.testing;
 
+/// Util
+fn alignedStringLength(n: usize) usize {
+    return 4 * (1 + @divFloor(n, 4));
+}
+
+fn alignedBlobLength(n: usize) usize {
+    return 4 * (@divFloor(n + 3, 4));
+}
+
 /// Supported OSC types
 pub const Type = enum { i, f, s, b, h, t, d, S, c, r, m, T, F, N, I };
 
@@ -72,40 +81,124 @@ pub const Value = union(Type) {
         }
     }
 
-    pub fn decode(comptime T: Type, buf: []const u8, value: *Value) !usize {
+    fn decodeString(buf: []const u8, value: *Value, comptime field_name: []const u8) !usize {
+        if (std.mem.indexOfScalar(u8, buf, 0)) |len| {
+            value.* = @unionInit(Value, field_name, buf[0..len]);
+            return alignedStringLength(len);
+        }
+        return error.InvalidString;
+    }
+
+    pub fn decode(T: Type, buf: []const u8, value: *Value) !usize {
         switch (T) {
-            .s, .S => |v| {
-                const len = std.mem.indexOfScalar(u8, buf, 0).?;
-                value.* = @unionInit(Value, @tagName(v), buf[0..len]);
-                return alignedStringLength(len);
-            },
-            .b => {
-                var tmp: Value = undefined;
-                const offset = try Value.decode(Type.i, buf, &tmp);
-                const len: usize = @intCast(tmp.i);
-                const size = offset + len;
-                value.* = Value{ .b = buf[offset..size] };
-                return offset + alignedBlobLength(len);
-            },
-            .T, .F, .N, .I => return 0,
-            .i, .f, .c, .r, .m => |v| {
-                value.* = @unionInit(Value, @tagName(v), @bitCast(std.mem.readInt(u32, buf[0..4], .big)));
-                return 4;
-            },
-            .h, .t, .d => |v| {
-                value.* = @unionInit(Value, @tagName(v), @bitCast(std.mem.readInt(u64, buf[0..8], .big)));
-                return 8;
+            inline else => |t| {
+                switch (t) {
+                    .s => return decodeString(buf, value, "s"),
+                    .S => return decodeString(buf, value, "S"),
+                    .b => {
+                        var tmp: Value = undefined;
+                        const offset = try Value.decode(Type.i, buf, &tmp);
+                        const len: usize = @intCast(tmp.i);
+                        const size = offset + len;
+                        value.* = Value{ .b = buf[offset..size] };
+                        return offset + alignedBlobLength(len);
+                    },
+                    .T, .F, .N, .I => {
+                        value.* = @unionInit(Value, @tagName(t), {});
+                        return 0;
+                    },
+                    .i, .f, .c, .r, .m => {
+                        value.* = @unionInit(Value, @tagName(t), @bitCast(std.mem.readInt(u32, buf[0..4], .big)));
+                        return 4;
+                    },
+                    .h, .t, .d => {
+                        value.* = @unionInit(Value, @tagName(t), @bitCast(std.mem.readInt(u64, buf[0..8], .big)));
+                        return 8;
+                    },
+                }
             },
         }
     }
 };
 
-fn alignedStringLength(n: usize) usize {
-    return 4 * (1 + @divFloor(n, 4));
-}
+pub const Message = struct {
+    address: []const u8,
+    typetag: []const u8,
+    values: []const Value,
 
-fn alignedBlobLength(n: usize) usize {
-    return 4 * (@divFloor(n + 3, 4));
+    /// Initialize a new message.
+    pub fn init(address: []const u8, typetag: []const u8, values: []const Value) Message {
+        return .{
+            .address = address,
+            .typetag = typetag,
+            .values = values,
+        };
+    }
+
+    /// Encode the message to OSC bytes
+    pub fn encode(self: *Message, buf: []u8) !usize {
+        const address = Value{ .s = self.address };
+        var offset = try address.encode(buf[0..]);
+        const typetag = Value{ .s = self.typetag };
+        if (typetag.s[0] != ',') {
+            buf[offset] = ',';
+            const tmp_offset = try typetag.encode(buf[1 + offset ..]);
+            offset += alignedStringLength(tmp_offset + 1);
+        } else {
+            offset += try typetag.encode(buf[offset..]);
+        }
+        for (self.values) |value| {
+            offset += try value.encode(buf[offset..]);
+        }
+        return offset;
+    }
+
+    /// Decode the message to zig types
+    pub fn decode(buf: []const u8, address: ?*[]const u8, typetag: ?*[]const u8, values: ?[]Value) !usize {
+        var tmp: Value = undefined;
+        var offset = try Value.decode(Type.s, buf[0..], &tmp);
+        if (address) |addr| {
+            addr.* = tmp.s;
+        }
+        offset += try Value.decode(Type.s, buf[offset..], &tmp);
+        if (tmp.s[0] != ',')
+            return error.InvalidTypetag;
+        if (typetag) |tag| {
+            tag.* = tmp.s[1..];
+        }
+        if (values) |vals| {
+            for (tmp.s[1..], 0..) |c, i| {
+                if (c == 'T' or c == 'F' or c == 'N' or c == 'I') continue;
+                const tag_name = [_]u8{c};
+                if (std.meta.stringToEnum(Type, &tag_name)) |T| {
+                    offset += try Value.decode(T, buf[offset..], &vals[i]);
+                } else {
+                    return error.InvalidType;
+                }
+            }
+        }
+        return offset;
+    }
+};
+
+test "message" {
+    const values = [_]Value{ .{ .i = 1234 }, .{ .f = 1.234 } };
+    var msg = Message.init("/foo/bar", "ifT", &values);
+    var buf: [64]u8 = undefined;
+    const num_encoded_bytes = try msg.encode(&buf);
+
+    try testing.expectEqual(@as(usize, 0), num_encoded_bytes % 4);
+
+    var address: []const u8 = undefined;
+    var typetag: []const u8 = undefined;
+    var out_values: [2]Value = undefined;
+
+    const num_decoded_bytes = try Message.decode(&buf, &address, &typetag, out_values[0..]);
+    try testing.expectEqual(num_encoded_bytes, num_decoded_bytes);
+    try testing.expectEqualSlices(u8, "/foo/bar", address);
+    try testing.expectEqualSlices(u8, "ifT", typetag);
+    try testing.expectEqual(@as(i32, 1234), out_values[0].i);
+    try testing.expectEqual(@as(f32, 1.234), out_values[1].f);
 }
 
 test "value encode/decode" {
