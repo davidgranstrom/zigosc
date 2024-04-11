c3ab7e2f30fdaaee983bd3847e5a09a320e89414
David Granström <david@davidgranstrom.com>
2024-04-11 14:19:37 +0200

    Export Timetag and BundleElement

--- src/root.zig
+++ src/root.zig
@@ -1,6 +1,8 @@
 pub const Value = @import("value.zig").Value;
 pub const Message = @import("message.zig").Message;
 pub const Bundle = @import("bundle.zig").Bundle;
+pub const BundleElement = @import("bundle.zig").BundleElement;
+pub const Timetag = @import("bundle.zig").Timetag;
 
 test {
     const std = @import("std");
