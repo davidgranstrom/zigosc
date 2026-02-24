2749e4311c387252bd13d58ec5aa47bfd958fa4e
David Granström <david@davidgranstrom.com>
2026-02-24 09:50:21 +0100

    Format with zig fmt

--- build.zig
+++ build.zig
@@ -14,7 +14,7 @@ pub fn build(b: *std.Build) void {
             .root_source_file = b.path("src/root.zig"),
             .target = target,
             .optimize = optimize,
-        })
+        }),
     });
 
     b.installArtifact(lib);
