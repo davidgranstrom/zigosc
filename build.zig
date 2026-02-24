6bd5b0ad3f823917fc683e4d6155291d2ee8d62f
sadguitarius <justbecuz@gmail.com>
2026-02-24 03:03:29 -0600

    fix building with Zig 0.15 (#5)
    * fix building with Zig 0.15
    
    * Format with zig fmt
    
    * Use zig 0.15.2 on CI
    
    * Fix action
    
    * Downgrade
    
    * Try again
    
    ---------
    
    Co-authored-by: David Granström <david@davidgranstrom.com>

--- build.zig
+++ build.zig
@@ -7,20 +7,25 @@ pub fn build(b: *std.Build) void {
         .root_source_file = b.path("src/root.zig"),
     });
 
-    const lib = b.addStaticLibrary(.{
+    const lib = b.addLibrary(.{
         .name = "zigosc",
-        .root_source_file = b.path("src/root.zig"),
-        .target = target,
-        .optimize = optimize,
+        .linkage = .static,
+        .root_module = b.createModule(.{
+            .root_source_file = b.path("src/root.zig"),
+            .target = target,
+            .optimize = optimize,
+        }),
     });
 
     b.installArtifact(lib);
     zigosc.linkLibrary(lib);
 
     const lib_unit_tests = b.addTest(.{
-        .root_source_file = b.path("src/root.zig"),
-        .target = target,
-        .optimize = optimize,
+        .root_module = b.createModule(.{
+            .root_source_file = b.path("src/root.zig"),
+            .target = target,
+            .optimize = optimize,
+        }),
     });
     const run_lib_unit_tests = b.addRunArtifact(lib_unit_tests);
     const test_step = b.step("test", "Run unit tests");
